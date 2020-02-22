e44cb9fc374cd235ed479d98aa120950f2c31f44
Giuseppe <giuscri@gmail.com>
2020-02-22 20:04:12 +0100

    solve some List exercises

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -37,6 +37,9 @@ object List { // `List` companion object. Contains functions for creating and wo
       case Cons(h,t) => Cons(h, append(t, a2))
     }
 
+  def append2[A](a1: List[A], a2: List[A]): List[A] =
+    foldRight(a1, a2) { (x,xs) => Cons(x, xs) }
+
   def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
     as match {
       case Nil => z
@@ -46,9 +49,23 @@ object List { // `List` companion object. Contains functions for creating and wo
   def sum2(ns: List[Int]) =
     foldRight(ns, 0)((x,y) => x + y)
 
+  def sum4(ns: List[Int]) =
+    foldLeft(ns, 0)((x,y) => x + y)
+
   def product2(ns: List[Double]) =
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
+  def product3(ns: List[Double]) =
+    foldRight(ns, 1.0) { (x,y) => x match {
+      case 0 => x
+      case x => x * y
+    }}
+
+  def product4(ns: List[Double]) =
+    foldLeft(ns, 0.0)((x,y) => x * y)
+
+  def length4[A](ns: List[A]): Int =
+    foldLeft(ns, 0)((x,_) => 1 + x)
 
   def tail[A](l: List[A]): List[A] = l match {
     case Cons(_, t) => t
@@ -78,9 +95,48 @@ object List { // `List` companion object. Contains functions for creating and wo
     case _ => Nil
   }
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int =
+    foldRight(l, 0) { (x,y) => 1 + y }
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  @annotation.tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+  }
+
+  def reverse[A](l: List[A]): List[A] =
+    foldLeft(l, Nil:List[A]) { (z, x) => Cons(x, z) }
+
+  def foldLeft2[A, B](l: List[A], z: B)(f: (B, A) => B): B = {
+    val ll = reverse(l)
+    foldRight(ll, z) { (x,y) => f(y,x) }
+  }
+
+  def foldRight2[A,B](l: List[A], z: B)(f: (A, B) => B): B = {
+    val ll = reverse(l)
+    foldLeft(ll, z) { (x,y) => f(y,x) }
+  }
 
   def map[A,B](l: List[A])(f: A => B): List[B] = ???
+
+  def listOfListsIntoSingleList[A](ll: List[List[A]]): List[A] = {
+    foldLeft(ll, Nil: List[A]) { (xs, ys) => append(xs, ys) }
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(init(List(1, 2, 3, 4)))
+    println(dropWhile(List(1, 2, 3, 4, 5)) { x => x < 2 })
+    println(product3(List(1, 2, 0, 2, 3)))
+    println(foldRight(List(1, 2, 3), Nil:List[Int])(Cons(_, _)))
+    println(length(List(1, 2, 3, 4, 5, 6)))
+    println(foldLeft(List(1, 2, 3), 0)((x,y) => x + y))
+    println(sum4(List(1, 2, 3)))
+    println(product4(List(1, 2, 3)))
+    println(length4(List(1, 2, 3, 4, 5, 6)))
+    println(reverse(List(1, 2, 3)))
+    println(foldRight2(List(1, 2, 3), Nil:List[Int])(Cons(_, _)))
+    println(foldLeft2(List(1, 2, 3), 0)((x,y) => x + y))
+    println(append2(List(1,2,3), List(4,5,6)))
+    println(listOfListsIntoSingleList(List(List(1,2,3), List(4,5,6), List(7,8,))))
+  }
 }
